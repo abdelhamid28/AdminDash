3eecbe3131ad413e38d41c9adc5fc6d4992c2216
abdelhamid28 <147178445+abdelhamid28@users.noreply.github.com>
2025-01-22 20:43:58 +0100

    Update app-pages.component.spec.ts

--- src/app/pages/app-pages/app-pages.component.spec.ts
+++ src/app/pages/app-pages/app-pages.component.spec.ts
@@ -1,11 +1,4 @@
-/*
-  Authors : initappz (Rahul Jograna)
-  Website : https://initappz.com/
-  App Name : Exam Full App Flutter
-  This App Template Source code is licensed as per the
-  terms found in the Website https://initappz.com/license
-  Copyright and Good Faith Purchasers © 2023-present initappz.
-*/
+
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AppPagesComponent } from './app-pages.component';
